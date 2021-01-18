9a529d2edb855031ef921b28250c2ca2eb1b3160
Ivan Andreev <ivandeex@gmail.com>
2021-01-18 21:06:00 +0300

    molecule: add focal image

--- molecule/runme.sh
+++ molecule/runme.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -x
 user=ivandeex
-tag_list="16.04 18.04"
-for tag in $tag_list ; do
+tags="16.04 18.04 20.04"
+for tag in $tags ; do
   docker build -f Dockerfile-$tag -t $user/molecule:$tag .
   docker push $user/molecule:$tag
 done
