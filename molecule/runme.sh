43435673c868952b521fe99764adba9222e35060
Ivan Andreev <ivandeex@gmail.com>
2022-01-17 00:19:58 +0300

    molecule: build images for amd64 and arm64

--- molecule/runme.sh
+++ molecule/runme.sh
@@ -2,7 +2,10 @@
 set -x
 user=ivandeex
 tags="16.04 18.04 20.04"
+docker buildx create --name molecule-builder --use
 for tag in $tags ; do
-  docker build -f Dockerfile-$tag -t $user/molecule:$tag .
-  docker push $user/molecule:$tag
+  docker buildx build \
+    --platform linux/amd64,linux/arm64 \
+    --tag $user/molecule:$tag --push \
+    --file Dockerfile-$tag .
 done
